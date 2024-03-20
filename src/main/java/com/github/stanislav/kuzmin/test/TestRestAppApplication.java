f639fa8adbc1aaf12548769e0b2989feee9e71dd
Stanislav <stason1986@mail.ru>
2024-03-21 00:14:56 +0300

    Initial commit

---
+++ src/main/java/com/github/stanislav/kuzmin/test/TestRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.stanislav.kuzmin.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/stanislav/kuzmin/test/TestRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stanislav.kuzmin.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
